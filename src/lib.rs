11b7b37801c48796f4a0508b443b53e5070d26c6
vlyr <vlyr@tuta.io>
2022-04-04 20:24:38 +0300

    get started with error type

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,28 @@
+use std::error::Error as StdError;
+use std::fmt;
+
 pub mod client;
 pub use client::{Client, Credentials};
+
+/// The Error enum. Used for handling Wilma-specific errors.
+#[derive(Debug)]
+pub enum Error {
+    InvalidCredentials,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use Error::*;
+
+        let msg = match self {
+            InvalidCredentials => "Invalid credentials were provided.",
+        };
+
+        write!(f, "{}", msg)
+    }
+}
+
+impl StdError for Error {}
 
 #[cfg(test)]
 mod tests {
